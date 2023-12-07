0c6f56b5d4350495a266073b735339b32fd512da
Vinicios <vinicios@sysout.com.br>
2023-12-07 15:25:09 -0300

    Feature de listar transferências

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Meu primeiro App'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-        'Hello World!',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
+      home: Scaffold(
+        body: TransferList(),
+        appBar: AppBar(
+          title: const Text('Transferências',
+              style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.green[900],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.telegram),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('Click'),
-      backgroundColor: Colors.red[600],
-    ),
-  ),
-));
+    ));
+
+class TransferList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransferItem(Transfer(100.0, 1000)),
+        TransferItem(Transfer(200.0, 2000)),
+        TransferItem(Transfer(300.0, 3000)),
+       ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.amount.toString()),
+        subtitle: Text(_transfer.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double amount;
+  final int accountNumber;
+
+  Transfer(this.amount, this.accountNumber);
+}
